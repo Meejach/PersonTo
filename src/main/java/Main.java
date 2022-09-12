6bdd417b76ad0f46cdd2e0d9465eee17128089c0
Meejach <deax0123@stud.kea.dk>
2022-09-12 10:10:43 +0200

    tilføjet get og set metoeder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args ) {
         PersonTo p1 = new PersonTo(1.73);
+        System.out.print(p1.getHøjde());
+        System.out.println();
+
+        p1.setHøjde(1.79);
+        System.out.print("ny højde " + p1.getHøjde());
 
     }
 }
--- src/main/java/PersonTo.java
+++ src/main/java/PersonTo.java
@@ -2,8 +2,22 @@ public class PersonTo {
 
 
     private double højde;
+    private int alder;
+
+
     public PersonTo(double højde) {
         this.højde = højde;
+    }
+
+    public double getHøjde() {
+        return højde;
+    }
+
+    public void setHøjde(double højde){
+        this.højde = højde;
+    }
+
+    public void harFødselsdag() {
 
     }
 }
